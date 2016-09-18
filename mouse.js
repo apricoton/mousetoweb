ad1fadc7d70a79e55c61def5ed38aedc85977e88
apricoton <apricoton.jp@gmail.com>
2016-09-18 23:36:49 +0900

    add yukkuri

--- mouse.js
+++ mouse.js
@@ -1,6 +1,7 @@
-let Mouse   = require('node-mouse'),
-    Dotenv  = require('dotenv').config(),
-    Twitter = require('twitter');
+const Mouse   = require('node-mouse'),
+      Dotenv  = require('dotenv').config(),
+      Twitter = require('twitter'),
+      exec    = require('child_process').exec;
 
 process.on('uncaughtException', (error) => {
     console.log(error);
@@ -14,21 +15,32 @@ let client = new Twitter({
     access_token_secret: process.env.TWITTER_OAUTH_ACCESS_SECRET,
 });
 
+let sentence = [
+    'にゃーん',
+    'にゃーん？',
+    '眠いにゃ',
+    'おなかすいたにゃ',
+    
+    
+];
+
 m.on('mouseup', (event) => {
     let status = '';
     if (event.button == 2) {
         // 右クリック
         status = '眠いにゃ';
+        exec('yukkuri "' + status + '"');
     } else if (event.button == 1) {
         // 中クリック
         status = 'おなかすいたにゃ';
     } else {
         // 左クリック
         status = 'にゃーん';
+        exec('yukkuri "' + status + '"');
     }
     
-    client.post('statuses/update', {status: status}, (error, tweet, response) => {
+    /*client.post('statuses/update', {status: status}, (error, tweet, response) => {
         if (error) throw error;
         console.log(tweet);
-    });
+    });*/
 });
